621db31c4bb7722f737c76f87c3c51a8220f90e1
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-03-31 19:48:46 -0300

    Atualizado

--- Estrutura-Sequencial/Exercicio02_sequencial.java
+++ Estrutura-Sequencial/Exercicio02_sequencial.java
@@ -12,18 +12,18 @@ import java.util.Scanner;
 
 public class Exercicio02_sequencial {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US); // Aponta que o padrão de digitação será americana
-        Scanner read = new Scanner(System.in); // Cria o Scanner
+        Locale.setDefault(Locale.US); 
+        Scanner read = new Scanner(System.in); 
 
-        System.out.print("Digite o valor do raio: "); // Imprime um comando para o usuário realizar uma ação
-        double raio = read.nextDouble(); // Solicita o valor do raio ao usuário
+        System.out.print("Digite o valor do raio: "); 
+        double raio = read.nextDouble(); 
 
-        double pi = 3.14159; // Informa o valor de pi
+        double pi = 3.14159; 
 
-        double area = pi * Math.pow(raio, 2); // Variável que armazena a fórmula da área do círculo
+        double area = pi * Math.pow(raio, 2); 
 
-        System.out.printf("A=%.4f %n", area); // Imprime o valor da área do círculo
+        System.out.printf("A=%.4f %n", area); 
 
-        read.close(); // Fecha o Scanner
+        read.close(); 
     }
 }
