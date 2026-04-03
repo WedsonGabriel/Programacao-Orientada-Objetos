d30a9f691842e0a4b93cf2faf330584f7128fc92
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-04-02 23:50:10 -0300

    Atualizado

---
+++ Beecrowd/Q_1001.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner read = new Scanner(System.in);
+        
+        int a = read.nextInt();
+        int b = read.nextInt();
+        
+        int x = a + b;
+        
+        System.out.println("X = " + x);
+        
+        read.close();
+    }
+}
