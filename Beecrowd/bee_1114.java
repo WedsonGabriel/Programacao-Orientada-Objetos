38c91ae5dc6e49a5056aad4cdb1f0033b72ca440
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-04-12 14:19:56 -0300

    Atualizado

---
+++ Beecrowd/bee_1114.java
@@ -0,0 +1,30 @@
+/*
+Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta
+informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a
+mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. 
+
+Entrada
+A entrada é composta por vários casos de testes contendo valores inteiros.
+
+Saída
+Para cada valor lido mostre a mensagem correspondente à descrição do problema.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        int senha = read.nextInt();
+
+        while(senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = read.nextInt();
+        }
+
+        System.out.println("Acesso Permitido");
+
+        read.close();
+    }
+}
