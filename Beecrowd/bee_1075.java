1bbc6b4304aef145f7775eab907714576f132393
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-04-12 13:58:23 -0300

    Atualizado

---
+++ Beecrowd/bee_1075.java
@@ -0,0 +1,27 @@
+/*
+Leia um valor inteiro N. Apresente todos os números entre 1 e 10000 que divididos por N dão resto igual a 2.
+
+Entrada
+A entrada contém um valor inteiro N (N < 10000).
+
+Saída
+Imprima todos valores que quando divididos por N dão resto = 2, um por linha.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        int number = read.nextInt();
+
+        for (int i = 1; i <= 10000; i++) {
+            if (i % number == 2) {
+                System.out.println(i);
+            }
+        }
+
+        read.close();
+    }
+}
