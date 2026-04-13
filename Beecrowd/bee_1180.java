3dd619bd0b269aa5ca02f47a908ab76b4ae3155b
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-04-13 15:49:32 -0300

    Atualizado

---
+++ Beecrowd/bee_1180.java
@@ -0,0 +1,42 @@
+/*
+Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N]. A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor e a sua posição dentro do vetor, 
+mostrando esta informação.
+
+Entrada
+A primeira linha de entrada contem um único inteiro N (1 < N < 1000), indicando o número de elementos que deverão ser lidos em seguida para o vetor X[N] de inteiros. A segunda linha contém cada
+um dos N valores, separados por um espaço. Vale lembrar que nenhuma entrada haverá números repetidos.
+
+Saída
+A primeira linha apresenta a mensagem “Menor valor:” seguida de um espaço e do menor valor lido na entrada. A segunda linha apresenta a mensagem “Posicao:” seguido de um espaço e da posição do vetor
+na qual se encontra o menor valor lido, lembrando que o vetor inicia na posição zero.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        int quantidade = read.nextInt();
+        int[] numeros = new int[quantidade];
+
+        for (int i = 0; i < quantidade; i++) {
+            numeros[i] = read.nextInt();
+        }
+
+        int menor = numeros[0];
+        int posicao = 0;
+
+        for (int i = 0; i < quantidade; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+                posicao = i;
+            }
+        }
+
+        System.out.println("Menor valor: " + menor);
+        System.out.println("Posicao: " + posicao);
+
+        read.close();
+    }
+}
